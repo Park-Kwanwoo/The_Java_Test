6715bb24b61a0acef676ee0ec797f7fbdabd9cf6
GwanWooPark <pgw4712@icloud.com>
2022-08-02 17:32:24 +0900

    Tested By Loop

---
+++ src/test/java/me/park/thejavatest/LoopTest.java
@@ -0,0 +1,81 @@
+package me.park.thejavatest;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.AggregateWith;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LoopTest {
+
+    @DisplayName("반복 테스트")
+//    반복 횟수와 반복 테스트 이름을 설정할 수 있다.
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void repeatTest(RepetitionInfo repetitionInfo) {
+        System.out.println("test" + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
+    }
+
+//    @DisplayName("파라미터 테스트")
+//    // 반복적인 테스트를 할 때마다 다른 값을 넣어서 테스트하고 싶을 때
+//    @ParameterizedTest(name = "{index} {displayName} message={0}")
+//    @ValueSource(ints = {10, 20, 30})
+////    @NullAndEmptySource
+//    void parameterizedTest(@ConvertWith(StudyConverter.class) Study study) {
+//        System.out.println(study.getLimit());
+//    }
+
+    static class StudyConverter extends SimpleArgumentConverter {
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType, "Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    @DisplayName("파라미터 테스트")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
+    void parameterizedTest(@AggregateWith(StudyAggregator.class) Study study) {
+        System.out.println(study);
+    }
+
+    // Aggregator 조건 : static inner class or public class
+    static class StudyAggregator implements ArgumentsAggregator {
+        @Override
+        public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
+            return new Study(accessor.getInteger(0), accessor.getString(1));
+        }
+    }
+
+
+    @BeforeAll
+    static void beforeAll() {
+
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+}
+
