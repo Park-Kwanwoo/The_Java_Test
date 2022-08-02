c101ecb3b7a3e7cd19e46319200221b460f79e88
GwanWooPark <pgw4712@icloud.com>
2022-08-02 16:37:13 +0900

    Tested By Condition

---
+++ src/test/java/me/park/thejavatest/ConditionTest.java
@@ -0,0 +1,45 @@
+package me.park.thejavatest;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+class ConditionTest {
+
+
+    @Test
+    @DisplayName("스터디 만들기")
+    @EnabledOnOs(OS.MAC)
+    void create_new_study() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println(test_env);
+        assumeTrue("LOCAL".equalsIgnoreCase(test_env));
+
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+
+}
