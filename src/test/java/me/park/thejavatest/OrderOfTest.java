922ca940253cb8197fef814595176c4b7ac404a7
GwanWooPark <pgw4712@icloud.com>
2022-08-04 00:06:00 +0900

    Order Of Test

---
+++ src/test/java/me/park/thejavatest/OrderOfTest.java
@@ -0,0 +1,48 @@
+package me.park.thejavatest;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderOfTest {
+
+    int value = 0;
+
+    @Order(2)
+    @Test
+    @DisplayName("스터디 만들기 fast")
+    void create_new_study() {
+        System.out.println(this);
+        System.out.println("fast later " + value++);
+
+    }
+
+    @Order(1)
+    @Test
+    @DisplayName("스터디 만들기 slow")
+    void create_new_study_again() {
+        System.out.println(this);
+        System.out.println("slow first " + value++);
+    }
+
+    @BeforeAll
+    void beforeAll() {
+
+        System.out.println("Before All");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("After All");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before Each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After Each");
+    }
+}
