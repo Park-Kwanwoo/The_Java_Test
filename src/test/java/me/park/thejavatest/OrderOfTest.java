db9364da5a46aee41b20eaf401804837627950fb
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-09 20:30:39 +0900

    JUnit 확장 모델

--- src/test/java/me/park/thejavatest/OrderOfTest.java
+++ src/test/java/me/park/thejavatest/OrderOfTest.java
@@ -1,10 +1,19 @@
 package me.park.thejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+// 선언을 통한 Extension 사용
+// Customizing 불가능
+//@ExtendWith(FindSlowTestExtension.class)
 public class OrderOfTest {
+
+    // 필드 정의 (static 필수)
+    @RegisterExtension
+    static FindSlowTestExtension findSlowTestExtension = new FindSlowTestExtension(1000L);
 
     int value = 0;
 
@@ -20,7 +29,8 @@ public class OrderOfTest {
     @Order(1)
     @Test
     @DisplayName("스터디 만들기 slow")
-    void create_new_study_again() {
+    void create_new_study_again() throws InterruptedException {
+        Thread.sleep(1005L);
         System.out.println(this);
         System.out.println("slow first " + value++);
     }
