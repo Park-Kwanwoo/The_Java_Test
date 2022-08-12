cdbf4581ff29e9ee7d358fa6a133c54fed015a21
Park-Kwanwoo <pgw4712@icloud.com>
2022-08-12 16:07:52 +0900

    Mockito Stubbing

--- src/main/java/me/park/thejavatest/domain/Study.java
+++ src/main/java/me/park/thejavatest/domain/Study.java
@@ -1,8 +1,38 @@
 package me.park.thejavatest.domain;
 
+import me.park.thejavatest.StudyStatus;
+
 public class Study {
 
     private Member owner;
+    private StudyStatus status;
+    private String name;
+
+    public Study(int limit, String name) {
+        this.name = name;
+        this.limit = limit;
+    }
+
+    public Study(int limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("limit은 0보다 커야 한다.");
+        }
+        this.limit = limit;
+    }
+
+    private int limit;
+
+    public StudyStatus getStatus() {
+        return this.status;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public Member getOwner() {
         return owner;
@@ -10,5 +40,14 @@ public class Study {
 
     public void setOwner(Member owner) {
         this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Study{" +
+                "status=" + status +
+                ", name='" + name + '\'' +
+                ", limit=" + limit +
+                '}';
     }
 }
--- src/test/java/me/park/thejavatest/study/StudyServiceTest.java
+++ src/test/java/me/park/thejavatest/study/StudyServiceTest.java
@@ -1,21 +1,58 @@
 package me.park.thejavatest.study;
 
+import me.park.thejavatest.domain.Member;
+import me.park.thejavatest.domain.Study;
 import me.park.thejavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
     @Test
-    void createStudyService(@Mock MemberService memberService,
+    void createNewStudy(@Mock MemberService memberService,
                             @Mock StudyRepository studyRepository) {
 
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
+
+        // 이러한 객체를 리턴받고 싶다 Stubbing
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("gwan@email.com");
+
+        // 호출이 되면 return 하라
+        // any() : 어떠한 값이 들어오든지 무조건 정해진 객체 반환
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+        assertEquals("gwan@email.com", memberService.findById(1L).get().getEmail());
+        assertEquals("gwan@email.com", memberService.findById(2L).get().getEmail());
+
+        // 예외를 던지게함
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+
+        memberService.validate(2L);
+
+        when(memberService.findById(any()))
+                .thenReturn(Optional.of(member))
+                .thenThrow(new RuntimeException())
+                .thenReturn(Optional.empty());
+
+        Optional<Member> byId = memberService.findById(1L);
+        assertThrows(RuntimeException.class, () -> {
+            memberService.findById(2L);
+        });
+        assertEquals(Optional.empty(), memberService.findById(3L));
     }
 }
